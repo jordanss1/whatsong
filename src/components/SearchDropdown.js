67627ba49048eb7cdfdaee1510dd6290220f5a93
jordanss1 <jordan.shade@live.com>
2022-09-16 19:25:44 +0100

    fixed dropdown and typestring state

--- src/components/SearchDropdown.js
+++ src/components/SearchDropdown.js
@@ -1,25 +1,15 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext } from "react";
 import SearchContext from "../contexts/SearchStore";
 
 const SearchDropdown = () => {
   const { setTypeString } = useContext(SearchContext);
-  const type = useRef("");
-
-  useEffect(() => {
-    if (!type.current) {
-      return;
-    } else {
-      console.log(type.current);
-      setTypeString(type.current);
-    }
-  }, [type.current]);
 
   return (
     <div className="ui form dropdownClass">
       <div className="field">
         <select
           onChange={({ target }) => {
-            type.current = target.value;
+            setTypeString(target.value);
           }}
           className="p-2"
         >
