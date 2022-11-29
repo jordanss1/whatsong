48947736b426e84fe35444072dd80892b19c9cdf
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2022-11-29 19:08:46 +0000

    working on Landing test

--- src/components/__tests__/Landing.test.js
+++ src/components/__tests__/Landing.test.js
@@ -1,11 +1,17 @@
-import React, { useContext } from "react";
+import React from "react";
 import { render, screen } from "@testing-library/react";
+import { useNavigate } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import SearchContext from "../../contexts/SearchStore";
 import Landing from "../Landing";
 
 test("On hover the classes are added to the spotify div", () => {
-  render(<Landing />);
+  const navigate = useNavigate();
+  render(
+    <SearchContext.Provider value={navigate}>
+      <Landing />
+    </SearchContext.Provider>
+  );
   screen.debug();
 });
