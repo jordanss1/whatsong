519c7ddc8e66f71f538a7bff310b045269575f7d
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2023-05-07 00:14:23 +0100

    added additional file for large types such as the api response

---
+++ src/types.ts
@@ -0,0 +1,61 @@
+export type ArtistDetailsType = {
+  external_urls: { spotify: string };
+  followers: { href: null; total: number };
+  genres: string[];
+  href: string;
+  id: string;
+  images: { height: number; url: string; width: number }[];
+  name: string;
+  popularity: number;
+  type: string;
+  uri: string;
+};
+
+export type AlbumDetailsType = {
+  album_group: string;
+  album_type: string;
+  artists: Partial<ArtistDetailsType>[];
+  available_markets: string[];
+  external_urls: { spotify: string };
+  href: string;
+  id: string;
+  images: { height: number; url: string; width: number }[];
+  name: string;
+  release_date: string;
+  release_date_precision: string;
+  total_tracks: number;
+  type: string;
+  uri: string;
+};
+
+export type TopTracksDetailsType = {
+  album: {
+    album_group: string;
+    album_type: string;
+    href: string;
+  };
+  artists: Partial<ArtistDetailsType>[];
+  available_markets?: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: { isrc: string };
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  is_local: boolean;
+  is_playable: boolean;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+};
+
+export type TrackAndArtistDetailsType =
+  | ArtistDetailsType[]
+  | Required<TopTracksDetailsType>[]
+  | { noItems: "noItems" };
