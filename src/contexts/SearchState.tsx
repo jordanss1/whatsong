40f3c51d39bde8a2219c728125f618747e9f2bb7
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2023-05-07 00:13:28 +0100

    Began to create new function to return the apps state and will add createContext and all types

--- src/contexts/SearchState.tsx
+++ src/contexts/SearchState.tsx
@@ -1,40 +1,36 @@
-import { Dispatch, useState, useRef } from "react";
+import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { spotifyTokenAndSearch, spotifyArtistAndAlbum } from "../api";
 import {
   useAnimateSearchManager,
   useAnimateListManager,
+  searchAnimateInit,
+  searchListAnimateInit,
 } from "./AnimateStateHooks";
-import { useArtistResults } from "./SearchResultHooks";
+import { useArtistResults, artistInitState } from "./SearchResultHooks";
+import { TopTracksDetailsType, TrackAndArtistDetailsType } from "../types";
 
 const SearchState = () => {
   const [term, setTerm] = useState<string>("");
   const [submittedTerm, setSubmittedTerm] = useState<string>("");
-  const [items, setItems] = useState([]);
-  const [selectedItem, setSelectedItem] = useState<null>(null);
+  const [items, setItems] = useState<TrackAndArtistDetailsType | null>(null);
+  const [selectedItem, setSelectedItem] =
+    useState<Required<TopTracksDetailsType> | null>(null);
   const [typeString, setTypeString] = useState<string>("");
   const [page, setPage] = useState<number>(1);
   const [slicedElements, setSlicedElements] = useState<number[]>([0, 10]);
   const [filteredAlbum, setFilteredAlbum] = useState<number>(0);
   const [filteredTrack, setFilteredTrack] = useState<number>(0);
 
-  const { animateStateSearch, setAnimateStateSearch } = useAnimateSearchManager(
-    {
-      initial: { opacity: 0.5, y: 100 },
-      exit: { opacity: 0, y: 0 },
-    }
+  const { animateStateSearch, setAnimateStateSearch } =
+    useAnimateSearchManager(searchAnimateInit);
+
+  const { animateStateList, setAnimateStateList } = useAnimateListManager(
+    searchListAnimateInit
   );
 
-  const { animateStateList, setAnimateStateList } = useAnimateListManager({
-    initial: { x: -300, opacity: 0 },
-    exit: { x: -300, opacity: 0 },
-  });
-
-  const { artist, albums, topTracks, setProfile } = useArtistResults({
-    artist: null,
-    albums: null,
-    topTracks: null,
-  });
+  const { artist, albums, topTracks, setProfile } =
+    useArtistResults(artistInitState);
 
   const focused = useRef<boolean>(false);
 
@@ -79,40 +75,40 @@ const SearchState = () => {
   return { ...providerValues };
 };
 
-export type UseSearchStateContext = ReturnType<typeof SearchState>
+export type UseSearchStateContext = ReturnType<typeof SearchState>;
 
 const initSearchContextState: UseSearchStateContext = {
-    animateStateSearch:  {}   ,
-    animateStateList:     {},
-    filteredAlbum:     0,
-    filteredTrack:     0,
-    topTracks:     {},
-    artist:     {},
-    albums:     [],
-    focused:    useRef(false),
-    pageChange:     useRef(false),
-    slicedElements:     [0,10],
-    page:     0,
-    typeString:     "",
-    term:     "",
-    selectedItem:     null,
-    submittedTerm:     "",
-    items:     [],
-    setAnimateStateList:     (initial: any, exit: any) => {},
-    setAnimateStateSearch:     (initial: any, exit: any) => {},
-    setFilteredAlbum:    () => {},
-    setFilteredTrack:    () => {},
-    setProfile:     () => {},
-    setSlicedElements:     () => {},
-    setPage:     () => {},
-    setTypeString:     () => {},
-    setTerm:     () => {},
-    setSubmittedTerm:    () => {},
-    setItems:     () => {},
-    spotifyTokenAndSearch:  ,
-    spotifyArtistAndAlbum:     ,
-    setSelectedItem:     ,
-    navigate:     ,
-}
+  animateStateSearch: searchAnimateInit,
+  animateStateList: searchListAnimateInit,
+  filteredAlbum: 0,
+  filteredTrack: 0,
+  topTracks: [],
+  artist: artistInitState.artist,
+  albums: [],
+  focused: useRef(false),
+  pageChange: useRef(false),
+  slicedElements: [0, 10],
+  page: 0,
+  typeString: "",
+  term: "",
+  selectedItem: null,
+  submittedTerm: "",
+  items: [],
+  setAnimateStateList: () => {},
+  setAnimateStateSearch: () => {},
+  setFilteredAlbum: () => {},
+  setFilteredTrack: () => {},
+  setProfile: () => {},
+  setSlicedElements: () => {},
+  setPage: () => {},
+  setTypeString: () => {},
+  setTerm: () => {},
+  setSubmittedTerm: () => {},
+  setItems: () => {},
+  spotifyTokenAndSearch: () => {},
+  spotifyArtistAndAlbum: () => {},
+  setSelectedItem: () => {},
+  navigate: () => {},
+};
 
 export default SearchState;
