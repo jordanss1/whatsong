c45b369981ecdd8651b0220e0c979b335c805083
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2023-06-12 16:10:13 +0100

    Added and modified styles to support new artist-cards and artist-detail component improvements

--- src/styles/inline.ts
+++ src/styles/inline.ts
@@ -2,30 +2,27 @@ export const gradient1 =
   "linear-gradient(to right, rgb(0, 0, 0) 11%, rgb(0, 0, 0, 0.6) 25%, rgb(0, 0, 0, 0.2) 50%, rgb(0, 0, 0, 0.6) 75%, rgb(0, 0, 0, 1) 100%),linear-gradient(to top,rgb(0, 0, 0) 0%,rgb(0, 0, 0) 7%, rgb(0, 0, 0, 0.8) 15%, rgb(0, 0, 0, 0.3) 25%,rgb(0, 0, 0, 0) 100%),linear-gradient(to bottom,rgb(0, 0, 0) 3%, rgb(0, 0, 0, 0.3) 25%, rgb(0, 0, 0, 0) 60%, rgb(0, 0, 0, 0) 100%),";
 
 export const gradient2 =
-  "linear-gradient(to right, rgb(0, 0, 0) 25%, rgb(0, 0, 0, 0.4) 40%, rgb(0, 0, 0, 0.4) 60%, rgb(0, 0, 0, 1) 75%),linear-gradient(to top, rgb(0, 0, 0) 17%, rgb(0, 0, 0, 0.9) 41%, rgb(0, 0, 0, 0) 50%),linear-gradient(to bottom,rgb(0, 0, 0) 22%, rgb(0, 0, 0, 0.2) 30%, rgb(0, 0, 0, 0) 60%, rgb(0, 0, 0, 0) 100%),";
-
-export const gradient3 =
-  "linear-gradient(to right, rgb(0, 0, 0) 32%, rgb(0, 0, 0, 0.4) 40%, rgb(0, 0, 0, 0.4) 50%, rgb(0, 0, 0) 67%),linear-gradient(to top, rgb(0, 0, 0) 50%, rgb(0, 0, 0, 0.3) 57%, rgb(0, 0, 0, 0) 70%),linear-gradient(to bottom,rgb(0, 0, 0) 19%, rgb(0, 0, 0, 0.2) 30%, rgb(0, 0, 0, 0) 60%, rgb(0, 0, 0, 0) 100%),";
+  "linear-gradient(to right, rgb(0, 0, 0) 10%, rgba(0, 0, 0, 0.4) 50%, rgba(0, 0, 0, 0.4) 60%, rgb(0, 0, 0) 85%), linear-gradient(to top, rgb(0, 0, 0) 10%, rgba(0, 0, 0, 0.9) 15%, rgba(0, 0, 0, 0) 40%), linear-gradient(rgb(0, 0, 0) 22%, rgba(0, 0, 0, 0.2) 30%, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0) 100%),";
 
 export const leftStyle =
-  "h-100 d-flex align-items-center justify-content-end arrows leftArrow";
+  "d-flex align-items-center justify-content-end arrows leftArrow";
 
 export const leftDisabledStyle =
-  "h-100 d-flex align-items-center justify-content-end arrows leftArrowDisabled";
+  "d-flex align-items-center justify-content-end arrows leftArrowDisabled";
 
-export const rightStyle = "h-100 d-flex align-items-center arrows rightArrow";
+export const rightStyle = "d-flex align-items-center arrows rightArrow";
 
 export const rightDisabledStyle =
-  "h-100 d-flex align-items-center arrows rightArrowDisabled";
+  "d-flex align-items-center arrows rightArrowDisabled";
 
 export const leftSmall =
   "d-flex align-items-center smallArrows leftSmallArrow w-100 pb-1";
 
 export const leftSmallDisabled =
-  " d-flex align-items-center smallArrows leftSmallArrowDisabled pb-1";
+  "d-flex align-items-center smallArrows leftSmallArrowDisabled pb-1";
 
 export const rightSmall =
-  " d-flex align-items-center justify-content-end smallArrows rightSmallArrow  w-100 pb-1";
+  "d-flex align-items-center justify-content-end smallArrows rightSmallArrow  w-100 pb-1";
 
 export const rightSmallDisabled =
   "d-flex align-items-center smallArrows rightSmallArrowDisabled pb-1";
